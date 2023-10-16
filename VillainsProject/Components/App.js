330db010fec12eebb776226559ebd23e43d2b50c
Emily Valdez <141756937+emily-valdez@users.noreply.github.com>
2023-10-16 13:45:59 -0500

    Initial commit - base files

---
+++ VillainsProject/Components/App.js
@@ -0,0 +1,17 @@
+import React from "react"
+import Header from "Header"
+import NavBar from "NavBar"
+import VillainsContainer from "VillainsContainer"
+
+
+function App() {
+    return (
+      <div className="app">
+        <Header />
+        <NavBar />
+        <VillainsContainer />
+      </div>
+    );
+  }
+  
+  export default App;
