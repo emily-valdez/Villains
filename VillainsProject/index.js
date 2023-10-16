63cb09e1c5238f0ce626e310e4cb5656acb913e8
Emily Valdez <emily.milan.valdez@gmail.com>
2023-10-16 16:26:54 -0500

    consolidated dupe files

--- VillainsProject/index.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "./index.css";
-import App from "./Components/App"
-import 'semantic-ui-css/semantic.min.css'
-
-
-ReactDOM.render(<App />, document.getElementById("root"));
